936b868052bd455d53428982affa106e513ed7e9
Ram-N <ramnarasimhan@gmail.com>
2013-12-30 23:09:37 -0800

    L3 updated

---
+++ R/L3_DataManipulation.R
@@ -0,0 +1,323 @@
+#' Intro to Data Analysis  
+#' R Code for Lecture 3
+#' @author Ram Narasimhan
+#'
+
+# Let's Read the input files for use later on.
+
+## CHANGE PATH TO THAT OF YOUR LAPTOP
+gdp <- read.csv("~/data/countries_wide.csv")
+names(gdp)
+
+## CHANGE PATH TO THAT OF YOUR LAPTOP
+getwd()
+santa <- read.csv("data/KCASANTA142.csv")
+names(santa)
+head(santa)
+dim(santa)
+
+# Split the Time column
+daytime <- strsplit(as.character(santa$Time), " ")
+library("plyr")
+df <- ldply(daytime)
+colnames(df) <- c("Date", "_Time")
+santa <- cbind(santa, df)
+str(santa)
+
+### 
+
+## CHANGE PATH TO THAT OF YOUR LAPTOP
+gdp <- read.csv("~/Documents/Personal/Analytics/data/countries_wide.csv")
+names(gdp)
+
+## CHANGE PATH TO THAT OF YOUR LAPTOP
+santa <- read.csv("~/data/KCASANTA142.csv")
+names(santa)
+head(santa)
+dim(santa)
+
+############################
+
+# Understanding NaN
+
+Inf/Inf #one way to create NaN
+x <- 4/0
+x/x
+class(x/x) #Note the type. Not a number is also of type numeric
+
+x <- 4/0
+y<- x/x
+is.nan(y) #This is how you detect a NaN
+# [1] TRUE
+
+
+#' Understanding NA's
+#' 
+#' 
+x <- c(3,4, NA, 6)
+x
+x[3]
+class(x[3]) #numeric
+is.na(x)
+na.omit(x)
+
+
+y <- c(TRUE, NA, FALSE, TRUE)
+class(y[2])
+
+
+####
+duplicated(iris)
+iris[143,]
+iris[with(iris, Sepal.Length==5.8),]
+
+
+## DEALING WITH NA's
+
+airquality$Ozone #make sure that we do see some NA's
+summary(airquality) #Notice that Summary tells us how many NA's there are
+
+#How many Ozone values are NA?
+sum(is.na(airquality$Ozone))
+
+#Print ROWS that DON'T have an NA for Ozone
+airquality[!is.na(airquality$Ozone) , ]
+
+#But wait, airquality$Solar.R has NA's too.
+na.omit(airquality)
+
+# USING COMPLETE CASES
+airquality[complete.cases(airquality), ]
+
+#Finally, let's convert these NA's to 0s
+#Note this is not always a good idea. 0s are very different from NA's
+tf <- is.na(airquality$Solar.R) # TRUE FALSE conditional vector
+airquality$Solar.R[tf] <- 0
+summary(airquality)
+
+
+
+# Split the Time column
+daytime <- strsplit(as.character(santa$Time), " ")
+library("plyr")
+df <- ldply(daytime)
+colnames(df) <- c("Date", "_Time")
+santa <- cbind(santa, df)
+str(santa)
+
+
+#################
+# TABLE
+names(movies)
+with(movies, table(year))
+with(movies, table(length))
+with(movies, table(length<200))
+
+
+#################
+
+library(datasets)
+str(UCBAdmissions)
+xtabs(~Gender, data=UCBAdmissions)
+?xtabs
+
+Convert to a data frame for easy cross tabulation
+DF <- as.data.frame(UCBAdmissions)
+## a data frame with a grid of factors and the counts in variable 'Freq'.
+DF
+xtabs(Freq~ Admit + Gender, data=DF)
+xtabs(Freq ~ ., DF)
+summary(xtabs(Freq ~ ., DF)) #This is one way to test for independence
+
+
+# C U T
+
+grouped.T <- cut(santa$TemperatureF, 10)
+summary(grouped.T)
+
+breaks=seq(0,100, by =10)
+nice.groups <- cut(santa$TemperatureF, breaks)
+summary(nice.groups)
+
+hml <- cut(santa$TemperatureF, breaks=c(0, 70, 90, 110), labels=c("Low","Medium","High"))
+summary(hml)
+### 
+# A P P L Y
+####
+
+dim(iris)
+iris[1,]
+iris
+apply(iris[,1:4], 1, mean)
+
+# Notice the contrast when using BY
+# By is a "group by" for different factors
+by(iris[, 1:4], Species, colMeans)
+
+
+# L A P P L Y
+
+l <- lapply(iris, class)
+s <- sapply(iris,class)
+
+lapply(airquality$Ozone, round)
+sapply(airquality$Ozone, round)
+
+sapply(airquality, max)
+head(airquality)
+
+
+# A P P L Y
+####
+
+dim(iris)
+iris[1,]
+iris
+apply(iris[,1:4], 1, mean)
+
+# Notice the contrast when using BY
+# By is a "group by" for different factors
+by(iris[, 1:4], Species, colMeans)
+
+
+# L A P P L Y
+
+l <- lapply(iris, class)
+s <- sapply(iris,class)
+
+lapply(airquality$Ozone, round)
+sapply(airquality$Ozone, round)
+
+sapply(airquality, max)
+head(airquality)
+# T A P P L Y
+x <- 1:20
+#A factor (of the same length!) defining groups:
+
+y <- factor(rep(c("CA","NY", "WA", "IL", "TX"), each = 4))
+#Add up the values in x within each subgroup defined by y:
+tapply(x, y, sum)  
+a  b  c  d  e  
+10 26 42 58 74 
+
+tapply(x,y)
+
+str(gdp)
+tapply(gdp$POP, gdp$country.isocode, mean)
+tapply(gdp$POP, list(gdp$country.isocode,gdp$year), mean)
+
+########################
+#AGGREGATE
+########################
+aggregate(Sepal.Length~Species, data=iris, FUN='length')
+aggregate(Species ~ Sepal.Length, data=iris, FUN='length')
+?aggregate
+
+#BONUS Example
+aggregate(state.x77, list(Region = state.region), mean)
+state.x77
+state.region
+
+############
+# D D P L Y 
+############
+library(plyr)
+#Let's use the in-built Titanic dataset to understand ddply
+#what does ddply do?
+# DDPLY: For each subset of a data frame, apply function then combine results into a data frame
+
+str(Titanic)
+titanic <- as.data.frame(Titanic)
+titanic
+ddply(titanic, .(Class), summarize, Total=sum(Freq))
+ddply(titanic, .(Class, Survived), summarize, Total=sum(Freq))
+ddply(titanic, .(Sex, Survived), summarize, Total=sum(Freq))
+ddply(titanic, .(Class, Sex, Survived), summarize, Total=sum(Freq))
+
+
+str(titanic)
+
+#---------------------------------------------
+#Extract Numbers from Strings
+x <-  '8 23 37 38 20 40 39 41 21 33'
+tokens <- strsplit(x, " ")
+str(tokens)
+vec <- unlist(tokens)
+str(vec)
+nums <- as.numeric(vec)
+str(nums)
+# or in one line
+nums <- as.numeric(unlist(strsplit(x," ")))
+str(nums)
+#---------------------------------------------
+
+
+## M E R G E
+x <- data.frame(hrs=c(3,4,7), values=1:3)
+x
+We want to fill in all missing hrs from 1 to 10, and fill in the value 0 for those.
+allhours <- data.frame(hours=1:10) 
+allhours
+
+merge(x, allhours, all=T, by.y="hours", by.x="hrs")
+df <- merge(x, allhours, all.y=TRUE, by=c("hrs"))
+df # convert the NA to 0
+df$values[is.na(df$values)] <- 0
+df
+
+
+
+### TIDY DATA
+##  RESHAPING
+############
+
+str(iris)
+iris
+dim(iris)
+names(iris)
+library(reshape2)
+
+m.iris <- melt(iris)
+names(m.iris)
+dcast(m.iris, Species~variable)
+
+m.iris <- melt(data=iris, id.var="Species")
+#Once you melt it this way, no way to "remember" which rows came from where.
+
+#need a row ID
+iris$id <- row.names(iris)
+irislong <- melt(iris, c("Species", "id"))
+dcast(irislong, Species+id~variable)
+
+gdp
+melt(gdp)
+m.gdp <- melt(gdp, id.vars=c("country","country.isocode", "year"))
+names(m.gdp)
+dcast(m.gdp, country.isocode+year ~ variable)
+
+## DATE AND TIME MANIPULATION
+as.Date('2013-10-30')
+dte <- c("02/27/13")
+as.Date(dte, "%m/%d/%y")
+
+# You can use ISOdate to combine elements and then convert to a date type
+ISOdate(year, month, day) and then as.Date()
+as.Date(ISOdate(2013,1,1))
+
+#The format() argument inside date
+#format(Sys.Date(), format=“%m/%d/%Y”
+#       Extracting Parts of a Date
+d <- as.Date("2013-10-15")
+p <- as.POSIXlt(d)
+# Get parts of p
+p$mon; p$year+1900
+
+
+s <- as.Date("2013-10-01")
+e <- as.Date("2013-11-01")
+#Creating a Sequence of Dates
+seq(from=s, to=e, by=1)
+seq(from=s, by='year', length.out=3)
+       
+
+
